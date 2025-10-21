1c1762e6735ee3f1e61bd7c9fa378e5e43802ce6
sorathpanzer <sorathpanzer>
2025-10-22 00:09:50 +0100

    up

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.github.ben-manes.versions") version "0.52.0"
+    id("com.github.ben-manes.versions") version "0.53.0"
 }
 
 tasks.register("clean", Delete::class) {
